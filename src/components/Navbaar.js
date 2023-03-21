eea93da812c9fddad0e9b99c7a5662066031b91e
NoisyBotDude <ashishsarmah11@gmail.com>
2023-03-21 09:32:28 +0530

    bug: fixed an error

--- src/components/Navbaar.js
+++ src/components/Navbaar.js
@@ -8,7 +8,7 @@ function Navbaar() {
             </div>
             <div className='col-md-8 d-flex align-items-center justify-content-around' style={{ backgroundColor: "#9fd2ca", height: "100%" }}>
                 <a href='/' className='text-decoration-none'><p className='m-0 ' style={{ fontSize: "20px", color: "#652f05" }}><b >Home</b></p></a>
-                <a href='/SearchResults' className='text-decoration-none'><p className='m-0' style={{ fontSize: "20px", color: "#652f05" }}><b>Search-Results</b></p></a>
+                {/* <a href='/SearchResults' className='text-decoration-none'><p className='m-0' style={{ fontSize: "20px", color: "#652f05" }}><b>Search-Results</b></p></a> */}
                 <a href='/Quiz' className='text-decoration-none'><p className='m-0' style={{ fontSize: "20px", color: "#652f05" }}><b>Quiz</b></p></a>
             </div>
         </div>
